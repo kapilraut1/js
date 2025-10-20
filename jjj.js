8a843f016a375854a3af71d70a127bee44d930f1
Kapil <kapilraut026@gmail.com>
2025-10-20 22:33:07 +0545

    hi

--- jjj.js
+++ jjj.js
@@ -1,3 +1,9 @@
-const arr = [1, 2, 3, 4, 5];
-arr.unshift(4);
-console.log(arr[1]);
+// const arr = [1, 2, 3, 4, 5];
+// arr.unshift(4);
+// console.log(arr[1]);
+const words = ["spray", "elite", "sxuberant", "destruction", "present"];
+
+const box = words.filter((e) => e.charAt(0) === "s");
+const check = words.map((e) => e.charAt(0) === "s");
+console.log(check);
+console.log(box);

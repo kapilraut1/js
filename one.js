42341abdac72c60984874584b8115ea154f3e770
Kapil <kapilraut026@gmail.com>
2025-10-22 20:18:18 +0545

    box

---
+++ one.js
@@ -0,0 +1,16 @@
+function Counter() {
+  let count = 0;
+
+  return function () {
+    count += 1;
+    return count;
+  };
+}
+
+const inc = Counter();
+console.log(inc());
+console.log(inc());
+console.log(inc());
+
+const dec = Counter();
+console.log(dec());

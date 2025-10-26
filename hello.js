daa7a0d85c2fd804ed850ef7bfedc8f8cebfb4b5
Kapil <kapilraut026@gmail.com>
2025-10-26 22:53:37 +0545

    hekl

--- hello.js
+++ hello.js
@@ -1,1 +1,27 @@
-I will learn and become the greatest of all timeo. Thank you.
+const products = [
+  {
+    title: "Cabbage",
+    id: 1,
+  },
+  {
+    title: "abbage",
+    id: 2,
+  },
+  {
+    title: "bage",
+    id: 3,
+  },
+];
+
+function box() {
+  const list = products.map((product) => (
+    <li key={product.id}>
+      console.log("Heeelo")
+      {product.title}
+    </li>
+  ));
+
+  return <ul>{list}</ul>;
+}
+
+box();
